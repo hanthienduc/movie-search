52601fec0978df6f4ade35d8aee2055059eea62e
hanthienduc <hanthienduc.96@gmail.com>
2022-08-31 21:49:41 +0900

    relocate core file to core folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { AppRoutes } from "./AppRoutes";
+import { AppRoutes } from "./core/AppRoutes";
 import { NavBar } from "./components/NavBar/NavBar";
 
 function App() {
--- src/core/AppRoutes.tsx
+++ src/core/AppRoutes.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { AnimationLayout } from "./AnimationLayout";
-import { Detail } from "./pages/Detail/Detail";
-import { Favorite } from "./pages/Favorite/Favorite";
-import { SearchMovie } from "./pages/Search/Search";
+import { Detail } from "../pages/Detail/Detail";
+import { Favorite } from "../pages/Favorite/Favorite";
+import { SearchMovie } from "../pages/Search/Search";
 
 export function AppRoutes() {
   return (
